9bc2946c2d0d0df35456ec615990d73b17cb44a6
Yixin <yixin0325@gmail.com>
2021-03-01 20:39:56 -0500

    create Laravel project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+
+    </head>
+    <body>
+        <div id="app">
+          <router-view></router-view>
+
+          <hr>
+
+          <router-link to="/">Home</router-link>
+          <router-link :to="{name: 'about'}">About</router-link>
+        </div>
+
+        <script src="/js/app.js">
+
+        </script>
+    </body>
+</html>
